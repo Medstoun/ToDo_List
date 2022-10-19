95f2bf461dced0fa718086b6b3d1806691a5b1cf
Medstoun <kirillmyagchilov@gmail.com>
2022-10-19 19:17:31 +0300

    commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist',
+    'rest_framework',
+    'social_django',
     'core',
 ]
 
