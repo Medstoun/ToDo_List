a3db2872d0b868be95f1fc0feb01101b7508a891
Medstoun <kirillmyagchilov@gmail.com>
2022-11-20 09:35:13 +0300

    commit

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 Stack: python3.10, Django, Postgres
 
 Установить зависимости командой "pip install -r requirements.txt"
+
+Приложение доступно по адресу "skytodo.ga"
+
+Все необходимые docker-образы загружены на сервер.
+
+Так же к приложению подключен Telegram-бот.
+
+Бот: @TooDoo_Listt_bot
+
+Для запуска проекта необходимо выполнить следующие команды на локальной машине:
+
+docker-compose up -d
+python manage.py migrate
---
+++ tests/cores/create_test.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_user_create(client, django_user_model):
+    data = {
+        'username': 'Testname',
+        'password': 'Pass123',
+        'password_repeat': 'Pass123',
+    }
+
+    response = client.post(
+        path='/core/signup',
+        data=data,
+        content_type='application/json'
+    )
+
+    user = django_user_model.objects.get(id=1)
+
+    expected_response = {
+        'id': user.id,
+        'username': user.username,
+        'password': user.password,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email
+    }
+
+    assert response.status_code == 201
+    assert response.data == expected_response
